e78047c692f4087a155566d579fe8984adef492d
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 12:04:56 +0600

    Argument vs Parameter

---
+++ argument.js
@@ -0,0 +1,29 @@
+function doubleIt (number){
+  const  doubled = number *2;
+  console.log(number, doubled);
+}
+
+console.log("=============");
+doubleIt(45),
+console.log("============");
+doubleIt(60);
+
+const mony = 122;
+
+doubleIt(mony)
+
+const tk = 177;
+doubleIt(tk);
+
+// difference
+
+function difference (num3, num4){
+    const diff = num3 - num4 ;
+    console.log(num3, num4 ,'difference', diff);
+}
+
+const myage = 59 ;
+
+const my = 54;
+
+difference(myage, my )
