1a348f4a52fa56b80b1698aef997693c12f6a9d3
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 09:09:01 +0600

    my lear is complete  function syntax

---
+++ function.js
@@ -0,0 +1,17 @@
+function myIntro (){
+    console.log("My Name is Mosharof");
+
+}
+myIntro();
+
+function myHouse (){
+    console.log("Bangladesh");
+}
+
+myHouse();
+
+
+function javaScript (){
+    console.log("Learning js");
+}
+javaScript();
