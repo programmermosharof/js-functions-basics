19499f2dc31b641f05e7c5a5ba45f7d6e6158c9d
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 11:12:52 +0600

    Function Parameter & Multiple Parameters '

---
+++ parameter.js
@@ -0,0 +1,38 @@
+
+
+
+
+function square (number){
+    console.log(number);
+
+    const all = number + number;
+    console.log(all);
+}
+square(4);
+
+
+function myname(name){
+    console.log(name);
+}
+myname("Mosharof");
+
+function add (a, b){
+    console.log(a+b);
+}
+add(100, 54)
+
+
+
+function total(num1, num2){
+    console.log(num1 + num2);
+}
+
+total(33, 67);
+
+function addAll(a, b, c, d){
+    const total = a + b+ c+ d ;
+    console.log("Total Tk:",total);
+    console.log(a, b, c, d);
+}
+
+addAll(50, 97, 49, 233)
