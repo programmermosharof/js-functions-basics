f11ec1c0ecef2bbd4c3c07a304cc80fc7dd0cc45
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 19:11:46 +0600

    module 17 is complete

--- even.js
+++ even.js
@@ -9,17 +9,36 @@ function evenonly(numbers){
 const  numbers = [5, 80, 35, 13, 22, 44, 60];
 evenonly(numbers);
 
-function geteven (num){
-    let even = [];
-    for (const nums of num ){
-        if(nums % 2 ===0){
-            even.push(nums);
 
-        }
+
+function getEvenNumbers(numbers){
+let even = [];
+
+for(const num of numbers){
+    if(num % 2 === 0 ){
+        even.push(num);
     }
-    return even;
+}
+return even;
 }
 
-const result = geteven([45, 22, 34, 65, 66, 80]);
-
+const result = getEvenNumbers([ 23, 45, 54, 65, 67, 40, 6, 36]);
 console.log(result);
+
+
+function getOdd(number){
+    let evens = [];
+    for (const nums of number){
+        if(nums % 2  === 1){
+            evens.push(nums);
+
+        }
+
+    }
+
+    return evens;
+}
+
+const all = getOdd([22, 23, 50, 68, 67, 99,]);
+
+console.log(all);
